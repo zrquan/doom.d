80bfb910405a7a151bc6b960f0b87c7ba00b272f
4shen0ne <4shen.01@gmail.com>
2021-12-19 17:32:29 +0800

    update chinese module

--- modules/input/chinese/packages.el
+++ modules/input/chinese/packages.el
@@ -2,6 +2,7 @@
 ;;; input/chinese/packages.el
 
 (package! youdao-dictionary)
+(package! go-translate)
 (package! pangu-spacing)
 (package! pyim :recipe (:host github :repo "tumashu/pyim"))
 (package! liberime :recipe (:host github :repo "merrickluo/liberime"))
