ca052118459607f19506fea898013187e6656e98
4shen0ne <4shen.01@gmail.com>
2024-02-02 14:50:17 +0800

    update packages.el

--- packages.el
+++ packages.el
@@ -1,25 +1,19 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
-;; To install a package with Doom you must declare them here and run 'doom sync'
-;; on the command line, then restart Emacs for the changes to take effect -- or
-;; use 'M-x doom/reload'.
-
 (package! org-roam-ui :pin "9474a254390b1e42488a1801fed5826b32a8030b")
+
 (package! org-modern
   :recipe (:host github :repo "minad/org-modern"))
 
 ;; https://github.com/org-roam/org-roam-bibtex#doom-emacs
-(package! org-roam-bibtex
-  :recipe (:host github :repo "org-roam/org-roam-bibtex"))
-;; When using org-roam via the `+roam` flag
+(package! org-roam-bibtex :recipe (:host github :repo "org-roam/org-roam-bibtex"))
 (unpin! org-roam)
 
 (package! dirvish)
 
-;; (package! emacs-codeql
-;;   :recipe (:host github :repo "anticomputer/emacs-codeql"))
+(package! magit-stats :recipe (:host github :repo "LionyxML/magit-stats"))
 
+;; (package! emacs-codeql :recipe (:host github :repo "anticomputer/emacs-codeql"))
 ;; (package! sicp :recipe (:host github :repo "webframp/sicp-info"))
-;; (package! magit-stats :recipe (:host github :repo "LionyxML/magit-stats"))
 ;; (package! org-super-agenda :recipe (:host github :repo "alphapapa/org-super-agenda"))
