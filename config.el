843c206092524e69b6ac97646c5948408ae5fd14
4shen0ne <4shen.01@gmail.com>
2024-07-27 22:11:01 +0800

    add nvd-dearch function

--- config.el
+++ config.el
@@ -3,12 +3,12 @@
 
 ;; Some functionality uses this to identify you.
 (setq! user-full-name "4shen0ne"
-      user-mail-address "4shen.01@gmail.com")
+       user-mail-address "4shen.01@gmail.com")
 
 (setq! auth-sources '("~/.authinfo" "~/.authinfo.gpg"))
 
 (setq! doom-localleader-key ","
-      doom-localleader-alt-key "M-,")
+       doom-localleader-alt-key "M-,")
 (map! :leader
       :desc "Kill buffer & window" "b x" #'kill-buffer-and-window
       :desc "Dirvish sidebar" "o o" #'dirvish-side
@@ -26,9 +26,9 @@
 
 ;; Evil
 (setq! evil-escape-key-sequence "df"
-      evil-snipe-override-evil-repeat-keys nil
-      evil-vsplit-window-right t
-      evil-split-window-below t)
+       evil-snipe-override-evil-repeat-keys nil
+       evil-vsplit-window-right t
+       evil-split-window-below t)
 
 (defun zrq/display-ansi-colors ()
   "Display ANSI color codes in current buffer"
@@ -39,6 +39,16 @@
   "Convert region from markdown to org, replacing selection"
   (interactive "r")
   (shell-command-on-region start end "pandoc -f markdown -t org" t t))
+
+(defun zrq/nvd-search ()
+  "Search NVD for the selected text."
+  (interactive)
+  (if (use-region-p)
+      (let* ((selection (buffer-substring-no-properties (region-beginning) (region-end)))
+             (query (url-hexify-string selection))
+             (url (concat "https://nvd.nist.gov/vuln/detail/" query)))
+        (browse-url url))
+    (message "No text selected.")))
 
 ;; (after! lsp-mode
 ;;   ;; https://github.com/emacs-lsp/lsp-mode/issues/3577#issuecomment-1709232622
