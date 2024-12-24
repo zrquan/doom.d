f8568d8e82edb598ab3995cc6975c546f0857a9b
4shen0ne <4shen.01@gmail.com>
2024-12-24 20:46:45 +0800

    pixel scroll

--- config.el
+++ config.el
@@ -143,3 +143,22 @@
       :desc "send request" "s" #'verb-send-request-on-point-other-window
       :desc "show vars" "x" #'verb-show-vars
       :desc "kill response buffers" "k" #'verb-kill-all-response-buffers)
+
+;; 像素级滚动
+;; https://emacs-china.org/t/topic/25114/5
+(pixel-scroll-precision-mode 1)
+(setq pixel-scroll-precision-interpolate-page t)
+(defun +pixel-scroll-interpolate-down (&optional lines)
+  (interactive)
+  (if lines
+      (pixel-scroll-precision-interpolate (* -1 lines (pixel-line-height)))
+    (pixel-scroll-interpolate-down)))
+
+(defun +pixel-scroll-interpolate-up (&optional lines)
+  (interactive)
+  (if lines
+      (pixel-scroll-precision-interpolate (* lines (pixel-line-height))))
+  (pixel-scroll-interpolate-up))
+
+(defalias 'scroll-up-command '+pixel-scroll-interpolate-down)
+(defalias 'scroll-down-command '+pixel-scroll-interpolate-up)
